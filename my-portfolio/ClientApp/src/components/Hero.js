65cab07400ece7fff85b59147b4dd3c7ce0f3287
Robert Miller <rpmiller4@gmail.com>
2023-03-27 00:34:02 -0700

    all changes to sync branches.

--- my-portfolio/ClientApp/src/components/Hero.js
+++ my-portfolio/ClientApp/src/components/Hero.js
@@ -6,10 +6,10 @@ const Hero = () => {
             <section className="bgimage" id="home">
             <div className="container-fluid">
                 <div className="row">
-                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
-                    <h2 className="hero_title">Hi, it's me Robert</h2>
-                    <p className="hero_desc">I am a professional software engineer in the west coast</p>
-                </div>
+                    <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
+                        <h2 className="hero_title">Robert Miller</h2>
+                        <p className="hero_desc">Painting with code to create software that's both functional and beautiful.</p>
+                    </div>
                 </div>
             </div>
             </section>
